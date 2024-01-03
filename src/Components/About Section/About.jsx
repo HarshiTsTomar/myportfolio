2d88102c7cc94b23f8c662ca76ab119310c75aa0
HarshiTsTomar <harshitt59@gmail.com>
2024-01-03 19:46:30 +0530

    updated Resume

--- src/Components/About Section/About.jsx
+++ src/Components/About Section/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './about.css'
 import {TbCloudDownload} from 'react-icons/tb'
-import img from '../../Assets/ht4.jpeg'
+import img from '../../Assets/ht3.jpeg'
 
 const About = () => {
   return (
@@ -27,7 +27,7 @@ const About = () => {
           </h4>
 
           <div className="aboutBtn">
-            <a href="HarshitCVs.pdf" download="HarshitCVs.pdf" className='flex'>
+            <a href="HarshitCVs.pdf" download="HarshitResume.pdf" className='flex'>
               Download CV <TbCloudDownload className='icon'/>
             </a>
           </div>
